6761c2258b8fc94910a3e52f94ce35528f883d31
András Ács <acsandras@gmail.com>
2018-11-22 11:27:00 +0100

    Nu med GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+
 public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    Button readButton;
+
+    @FXML
+    Button writeButton;
+
+    public void readFile() {
+        Readme x = new Readme();
+
+        textArea.setText(x.readme());
+    }
 }
